1af676ce6454b5ae7f9f2a01522a27392c5b50a4
Catalin Pit <catalinpit@gmail.com>
2018-11-20 14:15:15 +0000

    Added the Profile and Hobby models to the admin interface

--- MatchingSite/QMLove/admin.py
+++ MatchingSite/QMLove/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, Hobby
 
-# Register your models here.
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+# Only display the fields from the fieldsets
+# in the admin interface
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        ('Personal info', {'fields': ('username', 'password', 'first_name', 'last_name')}),
+    )
+    inlines = (ProfileInline,)
+
+class HobbyAdmin(admin.ModelAdmin):
+    model = Hobby
+    can_delete = False
+    verbose_name_plural = 'Hobbies'
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Hobby, HobbyAdmin)
--- MatchingSite/QMLove/models.py
+++ MatchingSite/QMLove/models.py
@@ -1,33 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from multiselectfield import MultiSelectField
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     #profileImage = models.ImageField(upload_to='profile_images')
-    name = models.CharField(max_length=200, blank=True)
     email = models.EmailField(max_length=254, blank=True)
-    gender = (
-        ('M', 'Male'),
-        ('F', 'Female')
-    )
+    gender = models.CharField(max_length=6, blank=True)
     dob = models.DateField(auto_now=False, auto_now_add=False, blank=True)
 
     def __str__(self):
-        return self.name
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+        return self.user.username
 
 class Hobby(models.Model):
-    hobby_name = (
+    HOBBIES = (
         ('1', 'Skiing'),
         ('2', 'Fishing'),
         ('3', 'Hunting'),
@@ -40,7 +26,8 @@ class Hobby(models.Model):
         ('10', 'Fashion'),
         ('11', 'Art')
     )
-    user = models.ManyToManyField(Profile)
+    hobby_name = MultiSelectField(choices=HOBBIES, null=True)
+    users = models.ManyToManyField(Profile)
 
     def __str__(self):
-        return self.hobby_name
+        return str(self.user)
