8be93be2c3366ea4eb0b7e12408ffa77003a1f09
cadiwal <cadiwal@gmail.com>
2018-11-21 21:46:44 +0000

    restration form
    submitting still doesnt work

--- MatchingSite/QMLove/forms.py
+++ MatchingSite/QMLove/forms.py
@@ -1,15 +1,30 @@
 from django import forms
 from QMLove.models import Profile
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
     class Meta:
-        model = Profile
-        fields = [
-                  'user',
-                  'password1', 
+        model = User
+        fields = ('username', 
+                  'first_name', 
+                  'last_name', 
                   'email', 
-                  'dob',
-                 ]
+                  'password1', 
+                  'password2')
         
-form = RegisterForm()
+        def save(self, commit=True):
+            user = super(RegisterationForm, self).save(commit=False)
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.email = self.cleaned_data['email']
+        
+            if commit:
+                user.save()
+   
+            return user
+        
+
+
--- MatchingSite/QMLove/views.py
+++ MatchingSite/QMLove/views.py
@@ -3,22 +3,25 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
-from QMLove.forms import RegisterForm
+from QMLove.forms import RegistrationForm
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    if request.method =="POST":
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            return redirect('/base.html')
     else:
-        form = UserCreationForm()
-    return render(request, 'QMLove/register.html', {'form': form})
+        form = RegistrationForm()
+        return render(request, 'QMLove/register.html',{'form':form})
+   
+
+
+
+            
+            
