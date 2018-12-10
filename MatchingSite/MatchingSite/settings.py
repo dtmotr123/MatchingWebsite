6edb787a1ad17b7605f7e56ca60f855bcf102b7c
dtmotr123 <dymoran97@gmail.com>
2018-12-10 13:47:58 +0000

    Login/Logout
    Added the Login/Logout. Only issue is the url tag. Doesn't work so the href is done manually. Not a good solution! Will keep looking for fix

--- MatchingSite/MatchingSite/settings.py
+++ MatchingSite/MatchingSite/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'MatchingSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'profile_images')
 MEDIA_URL = '/profile_images/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- MatchingSite/QMLove/urls.py
+++ MatchingSite/QMLove/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "QMLove"
 urlpatterns = [
     path('', views.index, name="index"),
     path('register/', views.register, name="register"),
+    path('', include('django.contrib.auth.urls')),
   
 ]
